a32f4c08fe26bdc67bd401017c11f83147e2d7a5
Paola Margarita Vasquez Juarez <paolavasquezdg1316@gmail.com>
2023-10-19 19:56:27 -0600

    Documentacion de API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API de Paola Margarita Vasquez Juarez",
+ *      description="API de ejemplo para clase de Desarrollo de BackEnd Y APIs",
+ *      @OA\Contact(
+ *          email="ia.paomvj@ufg.edu.sv",
+ *          name="Paola Margarita Vasquez Juarez",
+ *          url="https://localhost/api/"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/respuestaController.php
+++ app/Http/Controllers/respuestaController.php
@@ -6,7 +6,27 @@ use Illuminate\Http\Request;
 
 class respuestaController extends Controller
 {
-    //
+//
+
+/**
+ * 
+ * Descripción de la prueba para la clase de Desarrollo de BackEdn y API desarrollo.
+ * @return \Illuminate\Http\Response
+ *
+ * @OA\Get(
+ *     path="/api/ejecutar",
+ *     tags={"ejecutar"},
+ *     summary="Este es un endpoint o metodo para obtener datos y mostrarlos al usuario",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Se devuelve todos los registros cuando la consulta cuando la consulta sea satisfactoria"
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="No se pudo ejectar la ejecución"
+ *     )
+ * ) 
+ */
 
     public function index(){
         return response() ->json([
